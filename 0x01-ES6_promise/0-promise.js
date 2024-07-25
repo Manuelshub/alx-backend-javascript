d8a659000e079dfaff4ea39657e2d6c79df8fd4d
Emmanuel <kcemmy03@gmail.com>
2024-07-25 19:25:28 +0100

    Added a whitespace to code

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,4 @@
 export default function getResponseFromAPI () {
-  const newPromise = new Promise((resolve, reject) => {});
+  const newPromise = new Promise((resolve, reject) => { });
   return newPromise;
 }
