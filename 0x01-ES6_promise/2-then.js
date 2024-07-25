fd96bfd5c9f727e5cee064f9e465e2ed57f2c251
Emmanuel <kcemmy03@gmail.com>
2024-07-25 22:06:17 +0100

    Added curly braces to console.log

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,5 +4,5 @@ export default function handleResponseFromAPI(promise) {
       { status: 200, body: 'Success' }
     ))
     .catch(() => Error())
-    .finally(() => console.log('Got a response from the API'));
+    .finally(() => {console.log('Got a response from the API');});
 }
