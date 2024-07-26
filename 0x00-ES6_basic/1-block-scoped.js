cbf41f1db1f020b8fa745cb31c5ddd22451959b5
Emmanuel <kcemmy03@gmail.com>
2024-07-26 18:53:15 +0100

    Task 1: Block scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  /* eslint no-empty: "error" */
+  if (trueOrFalse) {
+    // empty
+  }
+
+  return [task, task2];
+}
